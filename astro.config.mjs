1d34292ef9800426b9e252849ccb8c5fbcaa5d13
nekronos <edubuntu2002@gmail.com>
2024-01-27 20:01:39 -0500

    Webpage integration

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://nekronos-spn.github.io',
+	integrations: [
+		starlight({
+			title: 'Chronometer App',
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
