5ee795cc6d51d8d07422dea94371dd2308620fe5
nekronos <edubuntu2002@gmail.com>
2024-01-27 20:04:35 -0500

    Fix root folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nekronos-spn.github.io',
+	base: '/AndroidStudio-tutorial',
 	integrations: [
 		starlight({
 			title: 'Chronometer App',
